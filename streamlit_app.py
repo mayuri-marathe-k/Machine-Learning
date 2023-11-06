0fda684ebf3a97000e79920895f8124ea5c3a944
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-06 12:16:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write("Let's get started with ML")
+# pip install pystan
+# pip install prophet
+# from prophet import Prophet
+# import numpy as np
+import pandas as pd
+
+df = pd.read_csv("sample.csv")
+st.write(df.head())
