2c12b87614a696d2d0130ab6e4c01847ae2e95af
mayuri-marathe-k <mayuri.marathe@infocenter.io>
2023-11-06 14:32:56 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-# pip install pystan
-# pip install prophet
-# from prophet import Prophet
-# import numpy as np
+pip install pystan
+pip install prophet
+from prophet import Prophet
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv("sample.csv")
@@ -12,4 +12,21 @@ df[df['Leave Type'].isin(['Personal Time Off'])]
 # df['y'] = df['y'].astype(int)
 # df['ds'] = pd.to_datetime(df['ds'])
 df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
-st.write(df.head())
+m = Prophet()
+m.fit(df)
+future = m.make_future_dataframe(33, freq='D')
+# display(future)
+# future
+
+forecast = m.predict(future)
+# forecast
+# forecast['yhat'].sum()
+# fig1 = m.plot(forecast)
+# fig2 = m.plot_components(forecast)
+
+
+from prophet.plot import plot_plotly, plot_components_plotly
+plot_plotly(m, forecast)
+
+
+plot_components_plotly(m, forecast)
