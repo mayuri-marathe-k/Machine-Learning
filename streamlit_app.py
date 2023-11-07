c86636e0308c2cc9cf04aabe8b39dff65e380245
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 17:38:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ df[df['Leave Type'].isin(['Personal Time Off'])]
 
 df['ds'] = pd.to_datetime(df['ds'], errors='coerce',dayfirst=False, format='%Y-%m-%d').dt.date
 df['ds'] = pd.to_datetime(df['ds'], dayfirst=True)
-buffer = io.StringIO()
-df.info(buf=buffer)
-st.write(s)
+# buffer = io.StringIO()
+# df.info(buf=buffer)
+# st.write(s)
 df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
 st.write(df)
 # m = Prophet()
