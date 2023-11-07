f8bce49147ad93e4754aa8cb59489b0d9350271d
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 16:00:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 from prophet import Prophet
 
-df = pd.read_csv("sample.csv")
-df.dropna(how='any',axis=0)
-df[df['Status'].isin(['Approved'])]
-df[df['Leave Type'].isin(['Personal Time Off'])]
+# df = pd.read_csv("sample.csv")
+# df.dropna(how='any',axis=0)
+# df[df['Status'].isin(['Approved'])]
+# df[df['Leave Type'].isin(['Personal Time Off'])]
 # df['y'] = df['y'].astype(int)
 # df['ds'] = pd.to_datetime(df['ds'])
-df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
+# df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
 # m = Prophet()
 # m.fit(df)
 # future = m.make_future_dataframe(33, freq='D')
