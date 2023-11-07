7fa5ecec34c74635ab4b0fe457d476a3b0627b34
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 17:33:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,12 @@ df[df['Status'].isin(['Approved'])]
 df[df['Leave Type'].isin(['Personal Time Off'])]
 
 df['ds'] = pd.to_datetime(df['ds'], errors='coerce',dayfirst=False, format='%Y-%m-%d').dt.date
-
-df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
-st.write(df)
+buffer = io.StringIO()
+df.info(buf=buffer)
+s = buffer.getvalue()
+st.write(s)
+# df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
+# st.write(df)
 # m = Prophet()
 # m.fit(df)
 # future = m.make_future_dataframe(33, freq='D')
