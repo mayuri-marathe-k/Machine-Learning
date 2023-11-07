c9ae11470095c61db9eb37fa72fca3270496af54
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 17:04:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ df.dropna(how='any',axis=0)
 df[df['Status'].isin(['Approved'])]
 df[df['Leave Type'].isin(['Personal Time Off'])]
 # df['y'] = df['y'].astype(int)
-df['ds'] = pd.to_datetime(df['ds'],format='ISO8601')
+df['ds'] = pd.to_datetime(df['ds'],format='mixed', dayfirst=True)
 # df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
 st.write(df)
 # m = Prophet()
