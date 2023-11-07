2957705df8e0d43e68596ce3b55ef49313eb4fe3
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 16:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ df = pd.read_csv("sample.csv")
 df.dropna(how='any',axis=0)
 df[df['Status'].isin(['Approved'])]
 df[df['Leave Type'].isin(['Personal Time Off'])]
-df['y'] = df['y'].astype(int)
+# df['y'] = df['y'].astype(int)
 df['ds'] = pd.to_datetime(df['ds'])
 df.loc[(df['ds'] >= '2023-01-01') & (df['ds'] < '2023-11-30')]
 st.write(df)
