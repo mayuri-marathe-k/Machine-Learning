9b75530b39ebc2a2d36665b376fb2396296534d8
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-07 18:52:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ df[df['Status'].isin(['Approved'])]
 df[df['Leave Type'].isin(['Personal Time Off'])]
 
 
-pd.to_datetime(df['ds'], format='%Y-%m-%d', errors='coerce')
+pd.to_datetime(df['ds'], format='%Y-%m-%d')
 buffer = io.StringIO()
 df.info(buf=buffer)
 s = buffer.getvalue()
