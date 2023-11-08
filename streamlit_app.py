20da84559e78c98b163b86497bb76edfb218a00f
mayuri-marathe-k <132362618+mayuri-marathe-k@users.noreply.github.com>
2023-11-08 12:14:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,15 @@ df.replace([np.inf, -np.inf], np.nan)
 df.dropna(inplace=True)
 df.dropna(how='any',axis=0)
 df['y'] = df['y'].astype(int)
+pd.to_datetime(df['ds'], format='mixed')
 df[(df['Status'].isin(['Approved'])) & (df['Leave Type'].isin(['Personal Time Off']))]
 
 # leave = df[df['Leave Type'].isin(['Personal Time Off'])]
 # st.write(leave)
 
 
-df['ds'] = pd.to_datetime(df['ds'], format='mixed')
-st.write(df['ds'])
+# df['ds'] = pd.to_datetime(df['ds'], format='mixed')
+# st.write(df['ds'])
 buffer = io.StringIO()
 df.info(buf=buffer)
 s = buffer.getvalue()
